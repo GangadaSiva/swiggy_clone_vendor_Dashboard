0762a158f2db25773adc549674b94c845130c099
GangadaSiva <sivashiv08@gmail.com>
2024-09-18 22:11:03 +0530

    congif

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
-  }
+  base: './', // Set the base path to './' for relative paths
 });
